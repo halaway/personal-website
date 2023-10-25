3ef58c2846970eaf8459a310926630712024ce8f
halaway <31904474+halaway@users.noreply.github.com>
2023-10-25 12:11:06 -0400

    Images

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
         <ScrollView>
          <View style={someStyles.container}> 
             <ImageBackground style={someStyles.image}
-              source = {require("../green.jpeg")}>
+              source = {require("../ab.jpeg")}>
 
                 <Text style={someStyles.text}> Donnolis</Text>
                 <View style={{flex:1, alignItems:'center', marginTop:10}}> 
